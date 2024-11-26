3b5af8bae812f782eddbee2db33cc878c28b0dff
harshaperla <harshaperla@gmail.com>
2024-11-26 19:34:47 +0530

    public path fix

---
+++ src/constants/images.ts
@@ -0,0 +1,7 @@
+import coverBg from '../assets/images/cover-bg.jpg';
+import profileAvatar from '../assets/images/profile-avatar.jpg';
+
+export const PROFILE_IMAGES = {
+  COVER: coverBg,
+  AVATAR: profileAvatar
+};
