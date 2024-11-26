329b67897b83e2eb965afe0054ccb419debbbb45
harshaperla <harshaperla@gmail.com>
2024-11-26 19:33:54 +0530

    public path fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets')
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets')
     }
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
     assetsDir: 'assets',
     rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          icons: ['@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons']
-        }
+      input: {
+        main: path.resolve(__dirname, 'index.html')
       }
     }
   }
